fa4c33fa2b9a7ab139f49ef1b2f222e405be4630
BasedHamburger <shasha.kuznetsov@gmail.com>
2023-11-08 17:50:56 +0700

    Add project files.

---
+++ AVLTreeLibrary/AVLNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLTreeLibrary
+{
+    public class AVLNode
+    {
+        public int Key { get; set; }
+        public AVLNode Left { get; set; }
+        public AVLNode Right { get; set; }
+        public int Height { get; set; }
+
+        public AVLNode(int key)
+        {
+            Key = key;
+            Left = null;
+            Right = null;
+            Height = 1;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using AVLTreeLibrary;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AVLTree tree = new AVLTree();
+            tree.Insert(10);
+            tree.Insert(1);
+            tree.Insert(11);
+            tree.Insert(303);
+            tree.Insert(12);
+            tree.Insert(89);
+            tree.Insert(156);
+            bool found = tree.Find(10);
+            Console.WriteLine("Tree print:");
+            tree.Print();
+            tree.Delete(10);
+            Console.WriteLine("Tree Print after delete");
+            tree.Print();
+
+            tree += 1;
+            Console.WriteLine("Tree after +");
+            tree.Print();
+
+            AVLTree tree2 = new AVLTree();
+            tree2.Insert(10);
+            tree2.Insert(1);
+            tree2.Insert(11);
+            tree2.Insert(25);
+            Console.WriteLine("Second tree");
+            tree2.Print();
+            AVLTree mergedTree = tree + tree2;
+            Console.WriteLine("Merget trees");
+            mergedTree.Print();
+
+        }
+    }
+}
